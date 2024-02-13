99e44b21091b9a16e1ee43f8c5413901b98a1a05
Jck123 <jck.1.2.3.a@gmail.com>
2024-02-13 16:45:42 -0500

    Bot can now parse using only the respective IDs

--- vesl-member-manager/src/main/java/vesl/events/MessageEventListener.java
+++ vesl-member-manager/src/main/java/vesl/events/MessageEventListener.java
@@ -2,19 +2,18 @@ package vesl.events;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.EnumSet;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.IMentionable;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.channel.middleman.GuildChannel;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.managers.channel.concrete.TextChannelManager;
 
 public class MessageEventListener extends ListenerAdapter {
     User interactingUser = null;
@@ -30,47 +29,57 @@ public class MessageEventListener extends ListenerAdapter {
         if (messageText.startsWith(",")) {
             event.getMessage().addReaction(Emoji.fromUnicode("U+2611")).queue();
 
-            Boolean channelInverse = messageText.contains("-ci") ? true : false;
+            //Boolean channelInverse = messageText.contains("-ci") ? true : false;
 
             if (messageText.startsWith(",help")) {
                 event.getChannel().sendMessage("You've entered ',help'").queue();
-            } else if (messageText.startsWith(",channelroleallow")) {
-                event.getChannel().sendMessage("You've entered ',channelroleallow'").queue();
+            } else if (messageText.startsWith(",debugparse")) {
+                String output = "";
                 ArrayList<ArrayList<Object>> results = parseOptions(messageText, event.getGuild());
-                System.out.println("===== [ RESULTS  ] =====");
-                System.out.println("----- [  ROLES   ] -----");
+                //System.out.println("===== [ RESULTS  ] =====");
+                //System.out.println("----- [  ROLES   ] -----");
+                output += "===== [ RESULTS  ] =====\n----- [  ROLES   ] -----\n";
                 if (results.get(0) != null) {
+                    //System.out.println("Roles length" + results.get(0).size());
                     for (Object o : results.get(0)) {
+                        //System.out.println(o == null);
                         System.out.println(o.getClass().getSimpleName() + " | " + o);
+                        output += o.getClass().getSimpleName() + " | " + o + "\n";
                     }
                 }
-                System.out.println("----- [ CHANNELS ] -----");
+                //System.out.println("----- [ CHANNELS ] -----");
+                output += "----- [ CHANNELS ] -----\n";
                 if (results.get(1) != null) {
                     for (Object o : results.get(1)) {
-                        if (o.getClass().equals(String.class))
-                            System.out.println(o.toString());
-                        else
-                            System.out.println(o.getClass().getSimpleName()  + " | " + ((GuildChannel)o).getName());
+                        //System.out.println(o.getClass().getSimpleName()  + " | " + ((GuildChannel)o).getName());
+                        output += o.getClass().getSimpleName()  + " | " + ((GuildChannel)o).getName() + "\n";
                     }
                 }
-                System.out.println("----- [  PERMS   ] -----");
+                //System.out.println("----- [  PERMS   ] -----");
+                output += "----- [  PERMS   ] -----\n";
                 if (results.get(2) != null) {
                     for (Object o : results.get(2)) {
-                        System.out.println(((Permission)o).getName());
+                        //System.out.println(((Permission)o).getName());
+                        output += ((Permission)o).getName() + "\n";
                     }
                 }
-                System.out.println("----- [  ALLOW   ] -----");
+                //System.out.println("----- [  ALLOW   ] -----");
+                output += "----- [  ALLOW   ] -----\n";
                 if (results.get(3) != null) {
                     for (Object o : results.get(3)) {
-                        System.out.println(((Permission)o).getName());
+                        //System.out.println(((Permission)o).getName());
+                        output += ((Permission)o).getName() + "\n";
                     }
                 }
-                System.out.println("----- [   DENY   ] -----");
+                //System.out.println("----- [   DENY   ] -----");
+                output += "----- [   DENY   ] -----\n";
                 if (results.get(4) != null) {
                     for (Object o : results.get(4)) {
-                        System.out.println(((Permission)o).getName());
+                        //System.out.println(((Permission)o).getName());
+                        output += ((Permission)o).getName() + "\n";
                     }
                 }
+                event.getChannel().sendMessage(output).queue();
             } else if (messageText.startsWith(",channelroledeny")) {
                 event.getChannel().sendMessage("You've entered ',channelroledeny'").queue();
                 //RoleManager manager = event.getGuild().getRoleById(1205225542882951300L).getManager();
@@ -108,26 +117,52 @@ public class MessageEventListener extends ListenerAdapter {
                 List<String> tempList = Arrays.asList(strOptions.substring(start, end).split("\\s*,\\s*"));
                 for(String s : tempList) {
                     s = s.trim();
-                    if (!s.startsWith("<@"))            //Skip if not role or user(all of them start with this)
-                        continue;
-                    if (s.charAt(2) == '&')     //Indicates Role
-                        parsedRoles.add(guild.getRoleById(s.substring(3, s.length() - 1)));
-                    else                        //Indicates User
-                        parsedRoles.add(guild.retrieveMemberById(s.substring(2, s.length() - 1)).complete());
+                    IMentionable mention = null;
+                    if (s.startsWith("<@")) {
+                        if (s.charAt(2) == '&')     //Indicates Role
+                            mention = guild.getRoleById(s.substring(3, s.length() - 1));
+                        else                        //Indicates User
+                            mention = guild.retrieveMemberById(s.substring(2, s.length() - 1)).complete();
+                    } else {
+                        try {
+                            mention = guild.getRoleById(s);
+                        } catch (NumberFormatException e) {}
+
+                        if (mention == null) {
+                            try {   //Try to parse member instead if can't get role
+                                mention = guild.retrieveMemberById(s).complete();
+                            } catch (Exception e1) {}       //Do nothing(don't parse) if can't parse long
+                        }
+                    }
+
+                    if (mention != null)    //Make sure mention actually has a user, otherwise skip
+                        parsedRoles.add(mention);
                 }
-            
             } else {                //Parse singular entry
                 matcher.find(start);                            //Tries to find next non-whitespace after option indicator("role:")
                 start = matcher.end();
                 int end = strOptions.indexOf(' ', start);       //Tries to find the next space OR end of string
                 if (end == -1) end = strOptions.length();
                 String strRole = strOptions.substring(start, end).trim();
+                IMentionable mention = null;
                 if (strRole.startsWith("<@")) {         //Skip if not role or user(all of them start with this)
                     if (strRole.charAt(2) == '&')       //Indicates Role
-                        parsedRoles.add(guild.getRoleById(strRole.substring(3, strRole.length() - 1)));
+                        mention = guild.getRoleById(strRole.substring(3, strRole.length() - 1));
                     else                                //Indicates User
-                        parsedRoles.add(guild.retrieveMemberById(strRole.substring(2, strRole.length() - 1)).complete());
+                        mention = guild.retrieveMemberById(strRole.substring(2, strRole.length() - 1)).complete();
+                } else {
+                    try {       //Try to see if only number is present
+                        mention = guild.getRoleById(strRole);
+                    } catch (NumberFormatException e) {}
+
+                    if (mention == null)
+                        try {   //Try to parse member instead if can't get role
+                            mention = guild.retrieveMemberById(strRole).complete();
+                        } catch (Exception e1) {}    //Do nothing(don't parse) if can't parse long
                 }
+
+                if (mention != null)    //Make sure mention actually has a user or role(don't add null values)
+                    parsedRoles.add(mention);
             }
             options.set(0, parsedRoles);                //Sends off the ArrayList in a package with a cute lil bow
         }
@@ -143,9 +178,17 @@ public class MessageEventListener extends ListenerAdapter {
                 List<String> tempList = Arrays.asList(strOptions.substring(start, end).split("\\s*,\\s*"));     //Splits up list, using commas as seperators
                 for(String s : tempList) {                          //Parses each new seperate string
                     s = s.trim();                                   //Removes extra whitespaces
-                    if (!s.startsWith("<#")) continue;              //Verifies it's a channel, skips otherwise
+                    GuildChannel channel = null;
 
-                    parsedChannels.add(guild.getGuildChannelById(s.substring(2, s.length() - 1)));  //Adds to list as a GuildChannel object
+                    if (s.startsWith("<#"))                     //Verifies it's a channel, tries to parse number otherwise
+                        channel = guild.getGuildChannelById(s.substring(2, s.length() - 1));  //Adds to list as a GuildChannel object
+                    else
+                        try {               //Try as if only guild channel ID were input
+                            channel = guild.getGuildChannelById(s);
+                        } catch (NumberFormatException e) {}    //Do nothing if can't parse number
+
+                    if (channel != null)        //Verify channel is not null before adding
+                        parsedChannels.add(channel);
                 }
             
             } else {                //Parse singular entry
@@ -154,8 +197,16 @@ public class MessageEventListener extends ListenerAdapter {
                 int end = strOptions.indexOf(' ', start);   //Finds next whitespace OR end of string
                 if (end == -1) end = strOptions.length();
                 String strChannel = strOptions.substring(start, end).trim();
+                GuildChannel channel = null;
                 if (strChannel.startsWith("<#"))        //Verifies it's a channel, skips otherwise
-                    parsedChannels.add(guild.getGuildChannelById(strChannel.substring(2, strChannel.length() - 1)));
+                    channel = guild.getGuildChannelById(strChannel.substring(2, strChannel.length() - 1));
+                else
+                    try {       //Try as if only guild channel ID were input
+                        channel = guild.getGuildChannelById(strChannel);
+                    } catch (NumberFormatException e) {}    //Do nothing if can't parse number
+                
+                if(channel != null)             //Verify channel is not null before adding
+                    parsedChannels.add(channel);
             }
             options.set(1, parsedChannels);         //Sends off the ArrayList in a package with a cute lil bow
         }

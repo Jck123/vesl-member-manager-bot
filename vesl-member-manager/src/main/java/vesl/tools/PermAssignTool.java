df056cc63833591b485384bfe52a063756385425
Jck123 <jck.1.2.3.a@gmail.com>
2024-03-18 17:15:15 -0400

    Added basic functioning PermAssignTool.permsAdd()

--- vesl-member-manager/src/main/java/vesl/tools/PermAssignTool.java
+++ vesl-member-manager/src/main/java/vesl/tools/PermAssignTool.java
@@ -5,11 +5,30 @@ import java.util.Set;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.IPermissionHolder;
+import net.dv8tion.jda.api.entities.PermissionOverride;
 import net.dv8tion.jda.api.entities.channel.middleman.GuildChannel;
 
 public class PermAssignTool {
     public static int permsAdd(Guild guild, IPermissionHolder role, GuildChannel channel, Set<Permission> allow, Set<Permission> deny) {
-        return -1;
+        if (guild == null)
+            return -1;
+        if (role == null)
+            return -2;
+        if (channel == null)
+            return -3;
+        if ((allow == null || allow.isEmpty()) && (deny == null || deny.isEmpty()))
+            return -4;
+            
+        PermissionOverride oldPerms = channel.getPermissionContainer().getPermissionOverride(role);
+
+        if (oldPerms != null) {
+            allow.addAll(oldPerms.getAllowed());
+            deny.addAll(oldPerms.getDenied());
+        }
+
+        channel.getPermissionContainer().getManager().putPermissionOverride(role, allow, deny).complete();
+
+        return 0;
     }
 
     public static int permsClear(Guild guild, IPermissionHolder role, GuildChannel channel, Set<Permission> perms) {

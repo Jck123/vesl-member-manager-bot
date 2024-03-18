65df6ccaa3d3387a6fd9766bb53a8d67fb1eaee2
Jck123 <jck.1.2.3.a@gmail.com>
2024-03-18 17:17:15 -0400

    Removed unnecessary variable declaration(for now)

--- vesl-member-manager/src/main/java/vesl/App.java
+++ vesl-member-manager/src/main/java/vesl/App.java
@@ -1,7 +1,6 @@
 package vesl;
 
 import jakarta.json.Json;
-import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import vesl.events.MessageEventListener;
@@ -14,6 +13,6 @@ public class App {
         jdab.enableIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MEMBERS);
         jdab.addEventListeners(new MessageEventListener());
 
-        JDA jda = jdab.build();
+        jdab.build();
     }
 }

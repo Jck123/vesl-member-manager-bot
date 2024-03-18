fa3a1e11de023b29ae3ea23be8dba489217cc873
Jck123 <jck.1.2.3.a@gmail.com>
2024-03-18 17:12:11 -0400

    Added bare PermAssignToolTest

---
+++ vesl-member-manager/src/test/java/vesl/PermAssignToolTest.java
@@ -0,0 +1,32 @@
+package vesl;
+
+import org.junit.Test;
+
+public class PermAssignToolTest {
+    private static String CREDENTIALS_DIRECTORY_PATH = "/key.json";
+
+    @Test
+    public void testPermsAdd() throws InterruptedException {
+        
+    }
+
+    @Test
+    public void testPermsClear() throws InterruptedException {
+        
+    }
+    
+    @Test
+    public void testPermsSetAll() throws InterruptedException {
+        
+    }
+
+    @Test
+    public void testPermsAddAll() throws InterruptedException {
+
+    }
+
+    @Test
+    public void testPermsClearAll() {
+    
+    }
+}

e713f73dfae4b2dda71d11b5cbb4bef66fe74f92
Jck123 <jck.1.2.3.a@gmail.com>
2024-04-04 19:32:11 -0400

    App.java now accounts for ProcessCache and new ButtonEventListener

--- vesl-member-manager/src/main/java/vesl/App.java
+++ vesl-member-manager/src/main/java/vesl/App.java
@@ -1,20 +1,25 @@
 package vesl;
 
+import java.util.HashMap;
+
 import jakarta.json.Json;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
+import vesl.events.ButtonInteractionListener;
 import vesl.events.MessageEventListener;
 
 public class App {
     private static String CREDENTIALS_DIRECTORY_PATH = "/key.json";
-    public static void main( String[] args ) {
+    private static HashMap<String, PermAssignDataPack> ProcessCache = null;
+    public static void main(String[] args) {
         final String TOKEN = Json.createReader(App.class.getResourceAsStream(CREDENTIALS_DIRECTORY_PATH)).readObject().getString("api_key");
+        ProcessCache = new HashMap<String, PermAssignDataPack>();
         JDABuilder jdab = JDABuilder.createLight(TOKEN);
         jdab.enableIntents(GatewayIntent.GUILD_MEMBERS, GatewayIntent.MESSAGE_CONTENT);
         jdab.enableCache(CacheFlag.MEMBER_OVERRIDES);
-        jdab.addEventListeners(new MessageEventListener());
-
+        jdab.addEventListeners(new MessageEventListener(ProcessCache), new ButtonInteractionListener(ProcessCache));
+        
         jdab.build();
     }
 }
